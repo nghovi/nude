ff3a9c4f101c1511565c2eaf461e94be32a79082
TrungND <trung-nd@trente.asia>
2017-03-02 08:33:07 +0700

    refresh weekly screen

--- calendar-adr/src/main/java/trente/asia/calendar/services/calendar/WeeklyPageFragment.java
+++ calendar-adr/src/main/java/trente/asia/calendar/services/calendar/WeeklyPageFragment.java
@@ -1,121 +1,120 @@
 package trente.asia.calendar.services.calendar;
 
+import java.util.Date;
+import java.util.List;
+
+import org.json.JSONObject;
+
+import com.github.ksoichiro.android.observablescrollview.ObservableListView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.github.ksoichiro.android.observablescrollview.ObservableListView;
-import com.github.ksoichiro.android.observablescrollview
-        .ObservableScrollViewCallbacks;
-import com.github.ksoichiro.android.observablescrollview.ScrollState;
-
-import org.json.JSONObject;
-
-import java.util.Date;
-import java.util.List;
-
+import asia.chiase.core.util.CCBooleanUtil;
 import asia.chiase.core.util.CCDateUtil;
 import asia.chiase.core.util.CCNumberUtil;
+import trente.asia.android.activity.ChiaseActivity;
 import trente.asia.android.util.CsDateUtil;
 import trente.asia.calendar.R;
+import trente.asia.calendar.commons.defines.ClConst;
 import trente.asia.calendar.services.calendar.model.CalendarDayModel;
 import trente.asia.calendar.services.calendar.view.CalendarDayListAdapter;
 import trente.asia.calendar.services.calendar.view.CalendarView;
 import trente.asia.calendar.services.calendar.view.WeeklyCalendarDayView;
 import trente.asia.calendar.services.calendar.view.WeeklyCalendarHeaderRowView;
+import trente.asia.welfare.adr.activity.WelfareActivity;
 
 /**
  * WeeklyPageFragment
  *
  * @author TrungND
  */
-public class WeeklyPageFragment extends SchedulesPageListViewFragment
-        implements ObservableScrollViewCallbacks, CalendarView
-        .OnCalendarDaySelectedListener, WeeklyCalendarDayView
-        .OnDayClickListener {
+public class WeeklyPageFragment extends SchedulesPageListViewFragment implements ObservableScrollViewCallbacks,CalendarView.OnCalendarDaySelectedListener,WeeklyCalendarDayView.OnDayClickListener{
 
-    protected ObservableListView observableListView;
-    protected CalendarDayListAdapter adapter;
+	protected ObservableListView		observableListView;
+	protected CalendarDayListAdapter	adapter;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        if (mRootView == null) {
-            mRootView = inflater.inflate(R.layout.fragment_weekly_page,
-                    container, false);
-        }
-        return mRootView;
-    }
-
-    @Override
-    protected void initView() {
-        super.initView();
-        observableListView = (ObservableListView) getView().findViewById(R.id
-                .scr_calendar_day);
-        observableListView.setScrollViewCallbacks(this);
-        observableListView.setDivider(null);
-    }
-
-    protected void onLoadSchedulesSuccess(JSONObject response) {
-        super.onLoadSchedulesSuccess(response);
-    }
-
-    protected void updateObservableScrollableView() {
-        List<CalendarDayModel> displayedModels = getDisplayedDayForList();
-        adapter = new CalendarDayListAdapter(activity, R.layout
-                .item_calendar_day, displayedModels, this);
-        observableListView.setAdapter(adapter);
-    }
-
-    @Override
-    public void updateList(String dayStr) {
-        int selectedPosition = adapter.findPosition4Code(dayStr);
-        observableListView.setSelection(selectedPosition);
-    }
-
-    @Override
-    protected List<Date> getAllDate() {
-        List<Date> dates = CsDateUtil.getAllDate4Week(CCDateUtil.makeCalendar
-                (selectedDate), CCNumberUtil.toInteger(prefAccUtil.getSetting
-
-                ().CL_START_DAY_IN_WEEK));
-        return dates;
-    }
-
-    @Override
-    public void onScrollChanged(int scrollY, boolean firstScroll, boolean
-            dragging) {
-
-    }
-
-    @Override
-    public void onDownMotionEvent() {
-
-    }
-
-    @Override
-    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
-        if (scrollState == ScrollState.UP) {
-            for (WeeklyCalendarHeaderRowView rowView : lstHeaderRow) {
-                if (rowView.index != 0) {
-                    rowView.setVisibility(View.GONE);
-                }
-            }
-        } else if (scrollState == ScrollState.DOWN) {
-            for (WeeklyCalendarHeaderRowView rowView : lstHeaderRow) {
-                rowView.setVisibility(View.VISIBLE);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		if(mRootView == null){
+			mRootView = inflater.inflate(R.layout.fragment_weekly_page, container, false);
+		}else{
+            if(CCBooleanUtil.checkBoolean(((WelfareActivity)activity).dataMap.get(ClConst.IS_UPDATE_SCHEDULE))){
+                ((WelfareActivity)activity).dataMap.clear();
+                ((ChiaseActivity)activity).isInitData = true;
             }
         }
-    }
+		return mRootView;
+	}
 
-    @Override
-    public void onCalendarDaySelected(CalendarDayModel reportModel) {
+	@Override
+	protected void initView(){
+		super.initView();
+		observableListView = (ObservableListView)getView().findViewById(R.id.scr_calendar_day);
+		observableListView.setScrollViewCallbacks(this);
+		observableListView.setDivider(null);
+	}
 
-    }
+	protected void onLoadSchedulesSuccess(JSONObject response){
+		super.onLoadSchedulesSuccess(response);
+	}
 
-    @Override
-    public void onClick(View v) {
+	protected void updateObservableScrollableView(){
+		List<CalendarDayModel> displayedModels = getDisplayedDayForList();
+		adapter = new CalendarDayListAdapter(activity, R.layout.item_calendar_day, displayedModels, this);
+		observableListView.setAdapter(adapter);
+	}
 
-    }
+	@Override
+	public void updateList(String dayStr){
+		int selectedPosition = adapter.findPosition4Code(dayStr);
+		observableListView.setSelection(selectedPosition);
+	}
+
+	@Override
+	protected List<Date> getAllDate(){
+		List<Date> dates = CsDateUtil.getAllDate4Week(CCDateUtil.makeCalendar(selectedDate), CCNumberUtil.toInteger(prefAccUtil.getSetting
+
+		().CL_START_DAY_IN_WEEK));
+		return dates;
+	}
+
+	@Override
+	public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging){
+
+	}
+
+	@Override
+	public void onDownMotionEvent(){
+
+	}
+
+	@Override
+	public void onUpOrCancelMotionEvent(ScrollState scrollState){
+		if(scrollState == ScrollState.UP){
+			for(WeeklyCalendarHeaderRowView rowView : lstHeaderRow){
+				if(rowView.index != 0){
+					rowView.setVisibility(View.GONE);
+				}
+			}
+		}else if(scrollState == ScrollState.DOWN){
+			for(WeeklyCalendarHeaderRowView rowView : lstHeaderRow){
+				rowView.setVisibility(View.VISIBLE);
+			}
+		}
+	}
+
+	@Override
+	public void onCalendarDaySelected(CalendarDayModel reportModel){
+
+	}
+
+	@Override
+	public void onClick(View v){
+
+	}
 }
