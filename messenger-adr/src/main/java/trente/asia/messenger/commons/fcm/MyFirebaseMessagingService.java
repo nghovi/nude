8cfca02c7b53bbc20c6919eb3f7ee49dcb596131
hviet <hviet@trentes-MacBook-Pro.local>
2017-05-31 16:14:39 +0700

    Using LoganSquare to parse api

--- messenger-adr/src/main/java/trente/asia/messenger/commons/fcm/MyFirebaseMessagingService.java
+++ messenger-adr/src/main/java/trente/asia/messenger/commons/fcm/MyFirebaseMessagingService.java
@@ -1,5 +1,8 @@
 package trente.asia.messenger.commons.fcm;
 
+import java.io.IOException;
+
+import com.bluelinelabs.logansquare.LoganSquare;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -12,7 +15,6 @@ import android.net.Uri;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
-import asia.chiase.core.util.CCJsonUtil;
 import asia.chiase.core.util.CCStringUtil;
 import trente.asia.android.util.CsMsgUtil;
 import trente.asia.messenger.R;
@@ -42,9 +44,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
 		if(remoteMessage.getData().size() > 0){
 			/* Get notification data from FCM */
 			String notification = remoteMessage.getData().get("bodyData");
-            mNoticeType = CCStringUtil.toString(remoteMessage.getData().get("type"));
-            mParentKey = CCStringUtil.toString(remoteMessage.getData().get("parentKey"));
-            mKey = CCStringUtil.toString(remoteMessage.getData().get("key"));
+			mNoticeType = CCStringUtil.toString(remoteMessage.getData().get("type"));
+			mParentKey = CCStringUtil.toString(remoteMessage.getData().get("parentKey"));
+			mKey = CCStringUtil.toString(remoteMessage.getData().get("key"));
 			boolean isNotification = false;
 			if(WelfareConst.NotificationType.MS_NOTI_NEW_MESSAGE.equals(mNoticeType)){
 				if(MessageFragment.activeBoardId == null || !MessageFragment.activeBoardId.equals(mKey)){
@@ -65,26 +67,31 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
 
 	private void sendNotification(String notification, String noticeType, String key, String parentKey){
 
-		FcmNotificationModel model = CCJsonUtil.convertToModel(notification, FcmNotificationModel.class);
+		FcmNotificationModel model = null;
+		try{
+			model = LoganSquare.parse(notification, FcmNotificationModel.class);
+			Intent intent = new Intent(this, MainMsgActivity.class);
+			intent.putExtra(WelfareConst.NotificationReceived.USER_INFO_NOTI_TYPE, noticeType);
+			intent.putExtra(WelfareConst.NotificationReceived.USER_INFO_NOTI_PARENT_KEY, parentKey);
+			intent.putExtra(WelfareConst.NotificationReceived.USER_INFO_NOTI_KEY, key);
 
-		Intent intent = new Intent(this, MainMsgActivity.class);
-		intent.putExtra(WelfareConst.NotificationReceived.USER_INFO_NOTI_TYPE, noticeType);
-		intent.putExtra(WelfareConst.NotificationReceived.USER_INFO_NOTI_PARENT_KEY, parentKey);
-		intent.putExtra(WelfareConst.NotificationReceived.USER_INFO_NOTI_KEY, key);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			int requestID = (int)System.currentTimeMillis();
+			PendingIntent pendingIntent = PendingIntent.getActivity(this, requestID, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		int requestID = (int)System.currentTimeMillis();
-		PendingIntent pendingIntent = PendingIntent.getActivity(this, requestID, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+			String content = "";
+			if(!CCStringUtil.isEmpty(model.body_loc_key)){
+				content = CsMsgUtil.message(this, model.body_loc_key, model.body_loc_args);
+			}
+			Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder)new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher).setContentTitle(getString(R.string.app_name)).setContentText(content).setAutoCancel(true).setSound(defaultSoundUri).setContentIntent(pendingIntent);
 
-        String content = "";
-        if(!CCStringUtil.isEmpty(model.body_loc_key)){
-            content = CsMsgUtil.message(this, model.body_loc_key, model.body_loc_args);
-        }
-		Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-		NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder)new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.ic_launcher).setContentTitle(getString(R.string.app_name)).setContentText(content).setAutoCancel(true).setSound(defaultSoundUri).setContentIntent(pendingIntent);
+			NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+			notificationManager.notify(WelfareConst.NOTIFICATION_ID, notificationBuilder.build());
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 
-		NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-		notificationManager.notify(WelfareConst.NOTIFICATION_ID, notificationBuilder.build());
 	}
 
 }
