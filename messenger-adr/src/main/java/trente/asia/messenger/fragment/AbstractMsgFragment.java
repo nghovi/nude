d9a0d99ab3e7e938f787854095dcd824e7718572
TrungND <trung-nd@trente.asia>
2017-06-16 11:52:05 +0700

    check delete content

--- messenger-adr/src/main/java/trente/asia/messenger/fragment/AbstractMsgFragment.java
+++ messenger-adr/src/main/java/trente/asia/messenger/fragment/AbstractMsgFragment.java
@@ -6,6 +6,7 @@ import org.json.JSONObject;
 
 import trente.asia.android.define.CsConst;
 import trente.asia.messenger.BuildConfig;
+import trente.asia.messenger.R;
 import trente.asia.messenger.services.message.MessageFragment;
 import trente.asia.messenger.services.user.MsgLoginFragment;
 import trente.asia.welfare.adr.activity.WelfareFragment;
@@ -37,6 +38,10 @@ public class AbstractMsgFragment extends WelfareFragment{
 		String returnCd = response.optString(CsConst.RETURN_CODE_PARAM);
 		if(WfErrorConst.MS_ERR_CODE_DEPT_CHANGED.equals(returnCd) ||
 				WfErrorConst.MS_ERR_CODE_CONTENT_DELETED.equals(returnCd)){
+			if(WfErrorConst.MS_ERR_CODE_CONTENT_DELETED.equals(returnCd)){
+				alertDialog.setMessage(getString(R.string.msg_common_error_ms002));
+				alertDialog.show();
+			}
 			emptyBackStack();
 			gotoFragment(new MessageFragment());
 		}else{
