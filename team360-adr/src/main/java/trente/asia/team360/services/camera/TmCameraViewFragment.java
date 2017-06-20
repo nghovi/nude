4b0d8285adae93b8a00f95aaebe7c245c6e7b45a
takano-yasuhiro <takano-yasuhiro@trente.asia>
2017-06-20 09:02:11 +0700

    team360 : camera screen

--- team360-adr/src/main/java/trente/asia/team360/services/camera/TmCameraViewFragment.java
+++ team360-adr/src/main/java/trente/asia/team360/services/camera/TmCameraViewFragment.java
@@ -1,12 +1,21 @@
 package trente.asia.team360.services.camera;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import trente.asia.team360.R;
 import trente.asia.team360.common.fragments.AbstractTmFragment;
+import trente.asia.team360.databinding.FragmentMemberViewBinding;
+import trente.asia.team360.services.member.TmMemberViewFragment;
+import trente.asia.welfare.adr.models.UserModel;
 
 /**
  * Created by takano-yasuhiro on 2017/06/19.
@@ -14,10 +23,42 @@ import trente.asia.team360.common.fragments.AbstractTmFragment;
 
 public class TmCameraViewFragment extends AbstractTmFragment {
 
+    private final static int RESULT_CAMERA = 1001;
+
+    private Button btn;
+
+    private ImageView img;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_camera_view, container, false);
         return mRootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        img = (ImageView) view.findViewById(R.id.img_id_camera_view);
+
+        btn = (Button) view.findViewById(R.id.btn_id_start_camera);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, RESULT_CAMERA);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RESULT_CAMERA) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(bitmap);
+        }
     }
 
 
