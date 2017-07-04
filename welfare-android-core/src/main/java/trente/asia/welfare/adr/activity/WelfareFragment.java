5a15d9a738bd12184e797338884556b2c6d5be92
takano-yasuhiro <takano-yasuhiro@trente.asia>
2017-07-04 16:31:15 +0700

    common : welfare fragement api logic

--- welfare-android-core/src/main/java/trente/asia/welfare/adr/activity/WelfareFragment.java
+++ welfare-android-core/src/main/java/trente/asia/welfare/adr/activity/WelfareFragment.java
@@ -14,6 +14,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
