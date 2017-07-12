09195d2917afa1117ddea7968a7f6f2364e09584
tien-lt <tien-lt@trente.asia>
2017-07-12 17:36:48 +0700

    [TC] add - user select fragment

--- thankscard-adr/src/main/java/trente/asia/thankscard/services/posted/PostTCFragment.java
+++ thankscard-adr/src/main/java/trente/asia/thankscard/services/posted/PostTCFragment.java
@@ -39,135 +39,133 @@ import trente.asia.welfare.adr.utils.WfPicassoHelper;
  * Created by tien on 7/12/2017.
  */
 
-public class PostTCFragment extends AbstractTCFragment implements View.OnClickListener, SelectDeptFragment.OnSelectDeptListener {
+public class PostTCFragment extends AbstractTCFragment implements View.OnClickListener,SelectDeptFragment.OnSelectDeptListener{
 
-    private List<Template> templates;
-    private FragmentPostTcBinding binding;
-    private Template template;
-    private List<DeptModel> departments;
-    private DeptModel department;
-    private UserModel member;
+	private List<Template>			templates;
+	private FragmentPostTcBinding	binding;
+	private Template				template;
+	private List<DeptModel>			departments;
+	private DeptModel				department;
+	private UserModel				member;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        if (mRootView == null) {
-            binding = DataBindingUtil.inflate(inflater, R.layout.fragment_post_tc, container, false);
-            mRootView = binding.getRoot();
-        }
-        return mRootView;
-    }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		if(mRootView == null){
+			binding = DataBindingUtil.inflate(inflater, R.layout.fragment_post_tc, container, false);
+			mRootView = binding.getRoot();
+		}
+		return mRootView;
+	}
 
-    @Override
-    public int getFragmentLayoutId() {
-        return R.layout.fragment_post_tc;
-    }
+	@Override
+	public int getFragmentLayoutId(){
+		return R.layout.fragment_post_tc;
+	}
 
-    @Override
-    public boolean hasBackBtn() {
-        return true;
-    }
+	@Override
+	public boolean hasBackBtn(){
+		return true;
+	}
 
-    @Override
-    public boolean hasSettingBtn() {
-        return true;
-    }
+	@Override
+	public boolean hasSettingBtn(){
+		return true;
+	}
 
-    @Override
-    public int getFooterItemId() {
-        return 0;
-    }
+	@Override
+	public int getFooterItemId(){
+		return 0;
+	}
 
-    @Override
-    public int getTitle() {
-        return R.string.fragment_post_edit_title;
-    }
+	@Override
+	public int getTitle(){
+		return R.string.fragment_post_edit_title;
+	}
 
-    @Override
-    public void buildBodyLayout() {
-        template = new Template();
-        template.templateId = prefAccUtil.get(TcConst.PREF_TEMPLATE_ID);
-        template.templateUrl = prefAccUtil.get(TcConst.PREF_TEMPLATE_PATH);
-        binding.rltSelectDept.setOnClickListener(this);
-        binding.rltSelectUser.setOnClickListener(this);
-        binding.lnrSelectCard.setOnClickListener(this);
-    }
+	@Override
+	public void buildBodyLayout(){
+		template = new Template();
+		template.templateId = prefAccUtil.get(TcConst.PREF_TEMPLATE_ID);
+		template.templateUrl = prefAccUtil.get(TcConst.PREF_TEMPLATE_PATH);
+		binding.rltSelectDept.setOnClickListener(this);
+		binding.rltSelectUser.setOnClickListener(this);
+		binding.lnrSelectCard.setOnClickListener(this);
+	}
 
-    @Override
-    protected void initData() {
-        super.initData();
-        requestAccountInfo();
-        requestTemplate();
-        buildTemplate(false);
-    }
+	@Override
+	protected void initData(){
+		super.initData();
+		requestAccountInfo();
+		requestTemplate();
+		buildTemplate(false);
+	}
 
-    private void requestTemplate() {
-        JSONObject param = new JSONObject();
-        requestLoad(TcConst.API_GET_TEMPLATE, param, true);
-    }
+	private void requestTemplate(){
+		JSONObject param = new JSONObject();
+		requestLoad(TcConst.API_GET_TEMPLATE, param, true);
+	}
 
-    private void requestAccountInfo() {
-        JSONObject param = new JSONObject();
-        requestLoad(WfUrlConst.WF_ACC_INFO_DETAIL, param, true);
-    }
+	private void requestAccountInfo(){
+		JSONObject param = new JSONObject();
+		requestLoad(WfUrlConst.WF_ACC_INFO_DETAIL, param, true);
+	}
 
-    @Override
-    protected void successLoad(JSONObject response, String url) {
-        if (TcConst.API_GET_TEMPLATE.equals(url)) {
-            requestTemplateSuccess(response);
-        } else if (WfUrlConst.WF_ACC_INFO_DETAIL.equals(url)) {
-            departments = CCJsonUtil.convertToModelList(response.optString("depts"), DeptModel.class);
-            department = new DeptModel(CCConst.NONE, getString(R.string.chiase_common_none));
-            departments.add(0, department);
-            for (DeptModel dept : departments) {
-                if (CCCollectionUtil.isEmpty(dept.members)) {
-                    dept.members = new ArrayList<>();
-                }
-                member = new UserModel(CCConst.NONE, getString(R.string.chiase_common_none));
-                dept.members.add(0, member);
-            }
-        } else {
-            super.successLoad(response, url);
-        }
-    }
+	@Override
+	protected void successLoad(JSONObject response, String url){
+		if(TcConst.API_GET_TEMPLATE.equals(url)){
+			requestTemplateSuccess(response);
+		}else if(WfUrlConst.WF_ACC_INFO_DETAIL.equals(url)){
+			departments = CCJsonUtil.convertToModelList(response.optString("depts"), DeptModel.class);
+			department = new DeptModel(CCConst.NONE, getString(R.string.chiase_common_none));
+			departments.add(0, department);
+			for(DeptModel dept : departments){
+				if(CCCollectionUtil.isEmpty(dept.members)){
+					dept.members = new ArrayList<>();
+				}
+				member = new UserModel(CCConst.NONE, getString(R.string.chiase_common_none));
+				dept.members.add(0, member);
+			}
+		}else{
+			super.successLoad(response, url);
+		}
+	}
 
-    private void requestTemplateSuccess(JSONObject response) {
-        templates = CCJsonUtil.convertToModelList(response.optString("templates"), Template.class);
-    }
+	private void requestTemplateSuccess(JSONObject response){
+		templates = CCJsonUtil.convertToModelList(response.optString("templates"), Template.class);
+	}
 
-    private void buildTemplate(boolean keepCurrentMessage) {
-        WfPicassoHelper.loadImage(getContext(), BuildConfig.HOST + template.templateUrl, binding.imgCard, null);
-    }
+	private void buildTemplate(boolean keepCurrentMessage){
+		WfPicassoHelper.loadImage(getContext(), BuildConfig.HOST + template.templateUrl, binding.imgCard, null);
+	}
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.rlt_select_dept:
-                SelectDeptFragment fragment = new SelectDeptFragment();
-                gotoFragment(fragment);
-                fragment.setDepartments(departments, department);
-                log(department.deptName);
-                fragment.setCallback(this);
-                break;
-            case R.id.rlt_select_user:
-                break;
-            case R.id.lnr_select_card:
-                break;
-            default:
-                break;
-        }
-    }
+	@Override
+	public void onClick(View view){
+		switch(view.getId()){
+		case R.id.rlt_select_dept:
+			SelectDeptFragment fragment = new SelectDeptFragment();
+			gotoFragment(fragment);
+			fragment.setDepartments(departments, department);
+			log(department.deptName);
+			fragment.setCallback(this);
+			break;
+		case R.id.rlt_select_user:
+			break;
+		case R.id.lnr_select_card:
+			break;
+		default:
+			break;
+		}
+	}
 
-    @Override
-    public void onDoneClick(DeptModel deptModel) {
-        this.department = deptModel;
-        binding.deptName.setText(department.deptName);
-        if (WelfareConst.NONE.equals(department.key)) {
-            member = department.members.get(0);
-            binding.userName.setText(member.userName);
-        }
-    }
+	@Override
+	public void onDoneClick(DeptModel deptModel){
+		this.department = deptModel;
+		binding.deptName.setText(department.deptName);
+		member = department.members.get(0);
+		binding.userName.setText(member.userName);
+	}
 
-    private void log(String msg) {
-        Log.e("PostTc", msg);
-    }
+	private void log(String msg){
+		Log.e("PostTc", msg);
+	}
 }
---
+++ thankscard-adr/src/main/java/trente/asia/thankscard/services/posted/presenter/UserAdapter.java
@@ -0,0 +1,76 @@
+package trente.asia.thankscard.services.posted.presenter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import trente.asia.thankscard.BR;
+import trente.asia.thankscard.R;
+import trente.asia.thankscard.databinding.ItemDepartmentBinding;
+import trente.asia.thankscard.databinding.ItemUserBinding;
+import trente.asia.welfare.adr.models.DeptModel;
+import trente.asia.welfare.adr.models.UserModel;
+
+/**
+ * Created by tien on 7/12/2017.
+ */
+
+public class UserAdapter extends RecyclerView.Adapter<ViewHolder>{
+
+	private List<UserModel>	users;
+	private Context			context;
+	private UserModel		selectUser;
+	private OnUserAdapterListener callback;
+
+	public void setCallback(OnUserAdapterListener callback) {
+		this.callback = callback;
+	}
+
+	@Override
+	public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+		context = parent.getContext();
+		View view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false);
+		return new ViewHolder(view);
+	}
+
+	@Override
+	public void onBindViewHolder(ViewHolder holder, int position){
+		final ItemUserBinding binding = (ItemUserBinding)holder.getBinding();
+		final UserModel user = users.get(position);
+		binding.setVariable(BR.user, user);
+		binding.executePendingBindings();
+        if (selectUser != null && selectUser.key != null && selectUser.key.equals(user.key)) {
+            binding.imgRight.setVisibility(View.VISIBLE);
+        } else {
+            binding.imgRight.setVisibility(View.INVISIBLE);
+        }
+
+        binding.getRoot().setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				callback.onSelectUser(user);
+				selectUser = user;
+				notifyDataSetChanged();
+			}
+		});
+	}
+
+	@Override
+	public int getItemCount(){
+		return users == null ? 0 : users.size();
+	}
+
+	public void setDepartments(List<UserModel> users, UserModel selectUser){
+		this.users = users;
+        this.selectUser = selectUser;
+		notifyDataSetChanged();
+	}
+
+	public interface OnUserAdapterListener {
+		void onSelectUser(UserModel userModel);
+	}
+}
