7342af6fba98c752621bd313c61d569f507c74a2
tien-lt <tien-lt@trente.asia>
2017-08-16 17:45:17 +0700

    [MS] change message board, add screen select depts

---
+++ messenger-adr/src/main/java/trente/asia/messenger/services/user/SelectDeptFragment.java
@@ -0,0 +1,39 @@
+package trente.asia.messenger.services.user;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import trente.asia.messenger.R;
+import trente.asia.messenger.databinding.FragmentSelectDeptBinding;
+import trente.asia.messenger.fragment.AbstractMsgFragment;
+import trente.asia.messenger.services.user.view.DeptAdapter;
+
+/**
+ * Created by tien on 8/16/2017.
+ */
+
+public class SelectDeptFragment extends AbstractMsgFragment {
+    private FragmentSelectDeptBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (mRootView == null) {
+            binding = DataBindingUtil.inflate(inflater, R.layout.fragment_select_dept, container, false);
+            mRootView = binding.getRoot();
+        }
+        return mRootView;
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        initHeader(R.drawable.wf_back_white, getString(R.string.msg_select_department), null);
+        binding.rlvDept.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.rlvDept.setAdapter(new DeptAdapter());
+    }
+}
---
+++ messenger-adr/src/main/java/trente/asia/messenger/services/user/view/DeptAdapter.java
@@ -0,0 +1,38 @@
+package trente.asia.messenger.services.user.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import trente.asia.messenger.R;
+import trente.asia.welfare.adr.models.DeptModel;
+
+/**
+ * Created by tien on 8/16/2017.
+ */
+
+public class DeptAdapter extends RecyclerView.Adapter<ViewHolder> {
+    private List<DeptModel>  depts;
+
+    public void setDepts(List<DeptModel> depts) {
+        this.depts = depts;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_select_dept, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return depts == null ? 4 : depts.size();
+    }
+}
