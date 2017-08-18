126475b322d55fb521d7169d2c9ae691935f9df4
tien-lt <tien-lt@trente.asia>
2017-08-18 11:47:38 +0700

    [OL] create office letter module

---
+++ officeletter-adr/src/main/java/asia/trente/officeletter/commons/activities/MainActivity.java
@@ -0,0 +1,8 @@
+package asia.trente.officeletter.commons.activities;
+
+/**
+ * Created by tien on 8/18/2017.
+ */
+
+public class MainActivity {
+}
---
+++ officeletter-adr/src/main/java/asia/trente/officeletter/commons/defines/OLConst.java
@@ -0,0 +1,8 @@
+package asia.trente.officeletter.commons.defines;
+
+/**
+ * Created by tien on 8/18/2017.
+ */
+
+public class OLConst {
+}
