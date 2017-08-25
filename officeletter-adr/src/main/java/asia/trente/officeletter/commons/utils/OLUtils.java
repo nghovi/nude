5d601fbf40b58a03814346dbcd6726341107aa42
tien-lt <tien-lt@trente.asia>
2017-08-25 10:05:24 +0700

    [OL] fix show document

--- officeletter-adr/src/main/java/asia/trente/officeletter/commons/utils/OLUtils.java
+++ officeletter-adr/src/main/java/asia/trente/officeletter/commons/utils/OLUtils.java
@@ -1,11 +1,13 @@
 package asia.trente.officeletter.commons.utils;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Environment;
 import android.util.Log;
 
 import java.io.File;
 
+import asia.trente.officeletter.R;
 import asia.trente.officeletter.commons.defines.OLConst;
 import asia.trente.officeletter.services.document.listener.OnDownloadListener;
 import trente.asia.android.util.AndroidUtil;
@@ -17,139 +19,140 @@ import trente.asia.android.view.ChiaseDownloadFileDialog;
  * Created by tien on 8/18/2017.
  */
 
-public class OLUtils {
-    public static String getFilesFolderPath(){
-        File folder = new File(Environment.getExternalStorageDirectory(), OLConst.APP_FOLDER);
-        if(!folder.exists()){
-            folder.mkdirs();
-        }
-        return folder.getAbsolutePath();
-    }
+public class OLUtils{
 
-    public static void downloadFilePrivate(final Activity activity, String fileUrl, final OnDownloadListener callback) {
-        if(AndroidUtil.verifyStoragePermissions(activity)){
-            File file = new File(activity.getFilesDir(), OLConst.PDF_TEMP_NAME);
+	public static String getFilesFolderPath(){
+		File folder = new File(Environment.getExternalStorageDirectory(), OLConst.APP_FOLDER);
+		if(!folder.exists()){
+			folder.mkdirs();
+		}
+		return folder.getAbsolutePath();
+	}
 
-            if(file.exists()){
-                callback.onDownloadCompleted();
-            }else{
-                final ChiaseDownloadFileDialog dialog = ChiaseDownloadFileDialog.startDialog(activity);
-                DownloadFileManager.downloadFile(activity, fileUrl, file, new DownloadFileManager.OnDownloadListener() {
-                    @Override
-                    public void onStart(){
-                        Log.d("LOG", "START UPLOADING");
-                    }
+	public static void downloadFilePrivate(final Activity activity, String fileUrl, final OnDownloadListener callback){
+		if(AndroidUtil.verifyStoragePermissions(activity)){
+			File file = new File(activity.getFilesDir(), OLConst.PDF_TEMP_NAME);
+			final ProgressDialog dialog = new ProgressDialog(activity);
+			dialog.setMessage(activity.getString(R.string.ol_loading));
+			dialog.show();
 
-                    @Override
-                    public void onSetMax(final int max){
-                        activity.runOnUiThread(new Runnable() {
+			DownloadFileManager.downloadFile(activity, fileUrl, file, new DownloadFileManager.OnDownloadListener() {
 
-                            @Override
-                            public void run(){
-                                dialog.setMax(max);
-                            }
-                        });
-                    }
+				@Override
+				public void onStart(){
+					Log.d("LOG", "START UPLOADING");
+				}
 
-                    @Override
-                    public void onProgress(final int current){
-                        activity.runOnUiThread(new Runnable() {
+				@Override
+				public void onSetMax(final int max){
+					activity.runOnUiThread(new Runnable() {
 
-                            @Override
-                            public void run(){
-                                dialog.setCurrent(current);
-                            }
-                        });
-                    }
+						@Override
+						public void run(){
 
-                    @Override
-                    public void onFinishDownload(){
-                        activity.runOnUiThread(new Runnable() {
+						}
+					});
+				}
 
-                            @Override
-                            public void run(){
-                                // dialog.fileDownloaded();
-                            }
-                        });
-                    }
+				@Override
+				public void onProgress(final int current){
+					activity.runOnUiThread(new Runnable() {
 
-                    @Override
-                    public void onResponse(boolean isSuccess, final String path){
-                        dialog.dismiss();
-                        log(path);
-                        callback.onDownloadCompleted();
-                    }
-                });
-            }
-        }
-    }
+						@Override
+						public void run(){
 
-    public static void downloadFile(final Activity activity, String fileName, String fileUrl){
-        if(AndroidUtil.verifyStoragePermissions(activity)){
+						}
+					});
+				}
 
-            String filePath = OLUtils.getFilesFolderPath() + "/" + fileName;
-            File file = new File(filePath);
+				@Override
+				public void onFinishDownload(){
+					activity.runOnUiThread(new Runnable() {
 
-            if(file.exists()){
-                OpenDownloadedFile.downloadedFileDialog(file, activity);
-            }else{
-                final ChiaseDownloadFileDialog dialog = ChiaseDownloadFileDialog.startDialog(activity);
-                DownloadFileManager.downloadFile(activity, fileUrl, file, new DownloadFileManager.OnDownloadListener() {
-                    @Override
-                    public void onStart(){
-                        Log.d("LOG", "START UPLOADING");
-                    }
+						@Override
+						public void run(){
+							// dialog.fileDownloaded();
+						}
+					});
+				}
 
-                    @Override
-                    public void onSetMax(final int max){
-                        activity.runOnUiThread(new Runnable() {
+				@Override
+				public void onResponse(boolean isSuccess, final String path){
+					dialog.dismiss();
+					callback.onDownloadCompleted();
+				}
+			});
 
-                            @Override
-                            public void run(){
-                                dialog.setMax(max);
-                            }
-                        });
-                    }
+		}
+	}
 
-                    @Override
-                    public void onProgress(final int current){
-                        activity.runOnUiThread(new Runnable() {
+	public static void downloadFile(final Activity activity, String fileName, String fileUrl){
+		if(AndroidUtil.verifyStoragePermissions(activity)){
 
-                            @Override
-                            public void run(){
-                                dialog.setCurrent(current);
-                            }
-                        });
-                    }
+			String filePath = OLUtils.getFilesFolderPath() + "/" + fileName;
+			File file = new File(filePath);
 
-                    @Override
-                    public void onFinishDownload(){
-                        activity.runOnUiThread(new Runnable() {
+			if(file.exists()){
+				OpenDownloadedFile.downloadedFileDialog(file, activity);
+			}else{
+				final ChiaseDownloadFileDialog dialog = ChiaseDownloadFileDialog.startDialog(activity);
+				DownloadFileManager.downloadFile(activity, fileUrl, file, new DownloadFileManager.OnDownloadListener() {
 
-                            @Override
-                            public void run(){
-                                // dialog.fileDownloaded();
-                            }
-                        });
-                    }
+					@Override
+					public void onStart(){
+						Log.d("LOG", "START UPLOADING");
+					}
 
-                    @Override
-                    public void onResponse(boolean isSuccess, final String path){
-                        activity.runOnUiThread(new Runnable() {
+					@Override
+					public void onSetMax(final int max){
+						activity.runOnUiThread(new Runnable() {
 
-                            @Override
-                            public void run(){
-                                dialog.dismiss();
-                                OpenDownloadedFile.downloadedFileDialog(new File(path), activity);
-                            }
-                        });
-                    }
-                });
-            }
-        }
-    }
+							@Override
+							public void run(){
+								dialog.setMax(max);
+							}
+						});
+					}
 
-    private static void log(String msg) {
-        Log.e("OLUtils", msg);
-    }
+					@Override
+					public void onProgress(final int current){
+						activity.runOnUiThread(new Runnable() {
+
+							@Override
+							public void run(){
+								dialog.setCurrent(current);
+							}
+						});
+					}
+
+					@Override
+					public void onFinishDownload(){
+						activity.runOnUiThread(new Runnable() {
+
+							@Override
+							public void run(){
+								// dialog.fileDownloaded();
+							}
+						});
+					}
+
+					@Override
+					public void onResponse(boolean isSuccess, final String path){
+						activity.runOnUiThread(new Runnable() {
+
+							@Override
+							public void run(){
+								dialog.dismiss();
+								OpenDownloadedFile.downloadedFileDialog(new File(path), activity);
+							}
+						});
+					}
+				});
+			}
+		}
+	}
+
+	private static void log(String msg){
+		Log.e("OLUtils", msg);
+	}
 }
