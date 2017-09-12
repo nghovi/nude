61490f3af23cf2b4f1f0b1f080dc9a3366722ddc
tien-lt <tien-lt@trente.asia>
2017-09-12 13:51:28 +0700

    [TC] add confirm dialog change picture

--- thankscard-adr/src/main/java/trente/asia/thankscard/services/posted/dialog/LimitStickerDialog.java
+++ thankscard-adr/src/main/java/trente/asia/thankscard/services/posted/dialog/LimitStickerDialog.java
@@ -1,36 +1,31 @@
-package trente.asia.thankscard.services.posted.view;
+package trente.asia.thankscard.services.posted.dialog;
 
 import android.databinding.DataBindingUtil;
 import android.view.View;
 
 import trente.asia.thankscard.R;
 import trente.asia.thankscard.databinding.ChoosePictureDialogBinding;
+import trente.asia.thankscard.databinding.LimitStickerDialogBinding;
 import trente.asia.thankscard.fragments.dialogs.TCDialog;
 
 /**
  * Created by tien on 7/19/2017.
  */
 
-public class ChoosePictureDialog extends TCDialog {
+public class LimitStickerDialog extends TCDialog {
 
     @Override
     public int getDialogLayoutId() {
-        return R.layout.choose_picture_dialog;
+        return R.layout.limit_sticker_dialog;
     }
 
     @Override
     public void buildDialogLayout(View rootView) {
-        ChoosePictureDialogBinding binding = DataBindingUtil.bind(rootView);
+        LimitStickerDialogBinding binding = DataBindingUtil.bind(rootView);
         binding.btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 btnPositiveListener.onClick(view);
-            }
-        });
-        binding.btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                btnNegativeListener.onClick(view);
             }
         });
     }
