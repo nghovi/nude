2862eeef2e553d6137d200068fed707ea1c4c8a7
CuongNV <cuongnv@trente-office1.local>
2017-10-24 19:02:07 +0700

    highlight color when have holiday

--- calendar-adr/src/main/java/trente/asia/calendar/services/calendar/view/MonthlyCalendarDayView.java
+++ calendar-adr/src/main/java/trente/asia/calendar/services/calendar/view/MonthlyCalendarDayView.java
@@ -32,150 +32,150 @@ import trente.asia.welfare.adr.utils.WelfareUtil;
  *
  * @author TrungND
  */
-public class MonthlyCalendarDayView extends LinearLayout{
+public class MonthlyCalendarDayView extends LinearLayout {
 
-	private Context						mContext;
-	public TextView					txtDayLabel;
-	private RelativeLayout				lnrRowContent;
-	private DailyScheduleClickListener	mListener;
+    private Context mContext;
+    public TextView txtDayLabel;
+    private RelativeLayout lnrRowContent;
+    private DailyScheduleClickListener mListener;
 
-	public String						day;
-	public Date							date;
-	public int							dayOfTheWeek;
-	public List<ScheduleModel>			lstSchedule		= new ArrayList<>();
-	private int							periodNum;
-	private boolean						isToday			= false;
-	private TextView					txtHoliday;
-	private List<ScheduleModel>			schedules		= new ArrayList<>();
-	private List<TextView>				txtSchedules	= new ArrayList<>();
-	private int							maxMarginTop	= 0;
-	private List<Integer>				usedMargins		= new ArrayList<>();
-	private ImageView					imgBirthday;
+    public String day;
+    public Date date;
+    public int dayOfTheWeek;
+    public List<ScheduleModel> lstSchedule = new ArrayList<>();
+    private int periodNum;
+    private boolean isToday = false;
+    private TextView txtHoliday;
+    private List<ScheduleModel> schedules = new ArrayList<>();
+    private List<TextView> txtSchedules = new ArrayList<>();
+    private int maxMarginTop = 0;
+    private List<Integer> usedMargins = new ArrayList<>();
+    private ImageView imgBirthday;
 
-	public MonthlyCalendarDayView(Context context){
-		super(context);
-		this.mContext = context;
-	}
+    public MonthlyCalendarDayView(Context context) {
+        super(context);
+        this.mContext = context;
+    }
 
-	public MonthlyCalendarDayView(Context context, AttributeSet attrs){
-		super(context, attrs);
-		this.mContext = context;
-	}
+    public MonthlyCalendarDayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.mContext = context;
+    }
 
-	public MonthlyCalendarDayView(Context context, AttributeSet attrs, int defStyle){
-		super(context, attrs, defStyle);
-		this.mContext = context;
-	}
+    public MonthlyCalendarDayView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        this.mContext = context;
+    }
 
-	public void initialization(Date itemDate, DailyScheduleClickListener listener, boolean isWithoutMonth){
-		LayoutParams params = new LayoutParams(0, LayoutParams.MATCH_PARENT, 1);
-		this.setLayoutParams(params);
-		this.setMinimumHeight(WelfareUtil.dpToPx(48));
-		this.day = CCFormatUtil.formatDateCustom(WelfareConst.WF_DATE_TIME_DATE, itemDate);
-		this.date = itemDate;
-		this.mListener = listener;
+    public void initialization(Date itemDate, DailyScheduleClickListener listener, boolean isWithoutMonth) {
+        LayoutParams params = new LayoutParams(0, LayoutParams.MATCH_PARENT, 1);
+        this.setLayoutParams(params);
+        this.setMinimumHeight(WelfareUtil.dpToPx(48));
+        this.day = CCFormatUtil.formatDateCustom(WelfareConst.WF_DATE_TIME_DATE, itemDate);
+        this.date = itemDate;
+        this.mListener = listener;
 
-		this.setOnClickListener(new OnClickListener() {
+        this.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v){
-				if(mListener != null){
-					mListener.onDailyScheduleClickListener(date);
-				}
-			}
-		});
+            @Override
+            public void onClick(View v) {
+                if (mListener != null) {
+                    mListener.onDailyScheduleClickListener(date);
+                }
+            }
+        });
 
-		// txtHoliday = (TextView) this.findViewById(R.id.txt_id_row_holiday);
+        // txtHoliday = (TextView) this.findViewById(R.id.txt_id_row_holiday);
 
-		txtDayLabel = (TextView)this.findViewById(R.id.txt_id_day_label);
-		txtDayLabel.setText(new SimpleDateFormat("d").format(itemDate));
-		Calendar itemCalendar = CCDateUtil.makeCalendar(itemDate);
-		if(isWithoutMonth){
-			txtDayLabel.setTextColor(Color.GRAY);
-		}else{
-			if(CCDateUtil.isToday(itemDate)){
-				isToday = true;
-				txtDayLabel.setBackgroundResource(R.drawable.shape_background_today);
-				txtDayLabel.setTextColor(Color.WHITE);
-			}else{
-				if(Calendar.SUNDAY == itemCalendar.get(Calendar.DAY_OF_WEEK)){
-					txtDayLabel.setTextColor(Color.RED);
-				}else if(Calendar.SATURDAY == itemCalendar.get(Calendar.DAY_OF_WEEK)){
-					txtDayLabel.setTextColor(Color.BLUE);
-				}
-			}
-		}
+        txtDayLabel = (TextView) this.findViewById(R.id.txt_id_day_label);
+        txtDayLabel.setText(new SimpleDateFormat("d").format(itemDate));
+        Calendar itemCalendar = CCDateUtil.makeCalendar(itemDate);
+        if (isWithoutMonth) {
+            txtDayLabel.setTextColor(Color.GRAY);
+        } else {
+            if (CCDateUtil.isToday(itemDate)) {
+                isToday = true;
+                txtDayLabel.setBackgroundResource(R.drawable.shape_background_today);
+                txtDayLabel.setTextColor(Color.WHITE);
+            } else {
+                if (Calendar.SUNDAY == itemCalendar.get(Calendar.DAY_OF_WEEK)) {
+                    txtDayLabel.setTextColor(Color.RED);
+                } else if (Calendar.SATURDAY == itemCalendar.get(Calendar.DAY_OF_WEEK)) {
+                    txtDayLabel.setTextColor(Color.BLUE);
+                }
+            }
+        }
 
-		imgBirthday = (ImageView)findViewById(R.id.img_birthday);
+        imgBirthday = (ImageView) findViewById(R.id.img_birthday);
 
-		lnrRowContent = (RelativeLayout)this.findViewById(R.id.lnr_id_row_content);
-	}
+        lnrRowContent = (RelativeLayout) this.findViewById(R.id.lnr_id_row_content);
+    }
 
-	public void addSchedule(ScheduleModel scheduleModel){
-		schedules.add(scheduleModel);
-	}
+    public void addSchedule(ScheduleModel scheduleModel) {
+        schedules.add(scheduleModel);
+    }
 
-	public void showSchedules(){
-		Collections.sort(usedMargins);
+    public void showSchedules() {
+        Collections.sort(usedMargins);
 //		Collections.sort(lstSchedule, getScheduleComparator(false));
-		for(ScheduleModel scheduleModel : schedules){
-			showSchedule(scheduleModel);
-		}
-	}
+        for (ScheduleModel scheduleModel : schedules) {
+            showSchedule(scheduleModel);
+        }
+    }
 
-	private void showSchedule(ScheduleModel scheduleModel){
-		if(ScheduleModel.EVENT_TYPE_BIRTHDAY.equals(scheduleModel.eventType)){
-			setLayoutBirthday(scheduleModel);
-		}else{
-			lstSchedule.add(scheduleModel);
-			int marginTop = maxMarginTop;
-			while(usedMargins.contains(marginTop)){
-				marginTop = marginTop + ClConst.TEXT_VIEW_HEIGHT;
-			}
-			maxMarginTop = marginTop;
-			usedMargins.add(marginTop);
+    private void showSchedule(ScheduleModel scheduleModel) {
+        if (ScheduleModel.EVENT_TYPE_BIRTHDAY.equals(scheduleModel.eventType)) {
+            setLayoutBirthday(scheduleModel);
+        } else {
+            lstSchedule.add(scheduleModel);
+            int marginTop = maxMarginTop;
+            while (usedMargins.contains(marginTop)) {
+                marginTop = marginTop + ClConst.TEXT_VIEW_HEIGHT;
+            }
+            maxMarginTop = marginTop;
+            usedMargins.add(marginTop);
 
-			int width = getWidth();
-			TextView txtSchedule = MonthlyCalendarRowView.createTextView(getContext(), width, 0, scheduleModel, marginTop - ClConst.TEXT_VIEW_HEIGHT + WelfareUtil.dpToPx(6));
-			lnrRowContent.addView(txtSchedule);
-			txtSchedules.add(txtSchedule);
-		}
-	}
+            int width = getWidth();
+            TextView txtSchedule = MonthlyCalendarRowView.createTextView(getContext(), width, 0, scheduleModel, marginTop - ClConst.TEXT_VIEW_HEIGHT + WelfareUtil.dpToPx(6));
+            lnrRowContent.addView(txtSchedule);
+            txtSchedules.add(txtSchedule);
+        }
+    }
 
-	public void removeAllData(){
-		lnrRowContent.removeAllViews();
-		periodNum = 0;
-		maxMarginTop = 0;
-		lstSchedule.clear();
-		usedMargins.clear();
-		usedMargins.add(0);
-		schedules.clear();
-		txtSchedules.clear();
-		imgBirthday.setVisibility(View.INVISIBLE);
-	}
+    public void removeAllData() {
+        lnrRowContent.removeAllViews();
+        periodNum = 0;
+        maxMarginTop = 0;
+        lstSchedule.clear();
+        usedMargins.clear();
+        usedMargins.add(0);
+        schedules.clear();
+        txtSchedules.clear();
+        imgBirthday.setVisibility(View.INVISIBLE);
+    }
 
-	public void addPeriod(ScheduleModel scheduleModel, int marginTop){
-		if(maxMarginTop < marginTop && marginTop - maxMarginTop == ClConst.TEXT_VIEW_HEIGHT){
-			maxMarginTop = marginTop;
-		}
+    public void addPeriod(ScheduleModel scheduleModel, int marginTop) {
+        if (maxMarginTop < marginTop && marginTop - maxMarginTop == ClConst.TEXT_VIEW_HEIGHT) {
+            maxMarginTop = marginTop;
+        }
 
-		periodNum++;
-		usedMargins.add(marginTop);
-		lstSchedule.add(scheduleModel);
-	}
+        periodNum++;
+        usedMargins.add(marginTop);
+        lstSchedule.add(scheduleModel);
+    }
 
-	public void addPassivePeriod(ScheduleModel scheduleModel, int marginTop){
-		periodNum++;
-	}
+    public void addPassivePeriod(ScheduleModel scheduleModel, int marginTop) {
+        periodNum++;
+    }
 
-	public int getActivePeriodNum(){
-		while(usedMargins.contains(maxMarginTop + ClConst.TEXT_VIEW_HEIGHT)){
-			maxMarginTop += ClConst.TEXT_VIEW_HEIGHT;
-		}
-		return maxMarginTop;
-	}
+    public int getActivePeriodNum() {
+        while (usedMargins.contains(maxMarginTop + ClConst.TEXT_VIEW_HEIGHT)) {
+            maxMarginTop += ClConst.TEXT_VIEW_HEIGHT;
+        }
+        return maxMarginTop;
+    }
 
-	public void setLayoutBirthday(ScheduleModel scheduleModel){
-		imgBirthday.setVisibility(View.VISIBLE);
-	}
+    public void setLayoutBirthday(ScheduleModel scheduleModel) {
+        imgBirthday.setVisibility(View.VISIBLE);
+    }
 }
